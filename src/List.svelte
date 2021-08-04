7fcc418d0638167b76c06f5b6e1052b5d7bdd3d8
Orin Fletcher <orin.fletcher@gmail.com>
2021-08-04 14:07:46 -0700

    lists questions and answers

--- src/List.svelte
+++
@@ -1,7 +0,0 @@
-<div>
-  <h1>How can I use this thing? What type of things can I create?</h1>
-  <p>
-    Do I want to use this? Maybe for landing page but content and blog are on
-    Next?
-  </p>
-</div>
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,15 +1,6 @@
 <script>
-  let result;
-  let correctAnswer = "b";
-  let answers = ["a", "b", "c", "d"];
+  import Question from "./Question.svelte";
   let quiz = getQuiz();
-
-  function pickAnswer(answer) {
-    if (answer === correctAnswer) {
-      return (result = "Correct!");
-    }
-    result = "OOOPS";
-  }
 
   async function getQuiz() {
     const res = await fetch(
@@ -26,27 +17,12 @@
 
 <div>
   <button on:click={handleClick}>Get New Questions</button>
-  {#if result}
-    <h4>{result}</h4>
-  {:else}
-    <h5>Please pick an answer</h5>
-  {/if}
 
   {#await quiz}
     Loading...
   {:then data}
-    <h3>{data.results[0].question}</h3>
+    {#each data.results as question}
+      <Question {question} />
+    {/each}
   {/await}
-
-  {#each answers as answer}
-    <button on:click={() => pickAnswer(answer)}>
-      Answer {answer.toUpperCase()}
-    </button>
-  {/each}
 </div>
-
-<style>
-  h4 {
-    color: red;
-  }
-</style>
