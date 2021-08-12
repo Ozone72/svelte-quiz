703303844497cf416c42b8d86185b2bf925f32db
Orin Fletcher <orin.fletcher@gmail.com>
2021-08-12 12:20:16 -0700

    refactors App

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,6 @@
 <script>
 	import Quiz from "./Quiz.svelte";
-	export let name = "Suckah";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<Quiz />
-</main>
-
-<style>
-	:global(h4) {
-		color: green;
-	}
-</style>
+<h1>Cinematic Memory Quiz</h1>
+<Quiz />
